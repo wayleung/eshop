adfe561aa253bd57eab9a429ec077d725411a3e1
WayLeung <610686949@qq.com>
2018-07-03 13:36:20 +0800

    update the upload page and add the getFileList

--- src/main/java/com/way/action/FileAction.java
+++ src/main/java/com/way/action/FileAction.java
@@ -25,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -84,15 +85,23 @@ public class FileAction extends BaseAction {
 	 *
 	 */
 	@RequestMapping(value="/down",method=RequestMethod.GET)
-	public void down(HttpServletRequest request,HttpServletResponse response) throws IOException{
+	public void down(@RequestParam(value="file") String file,HttpServletRequest request,HttpServletResponse response) throws IOException{
+		
 		//模拟文件,myfile.txt 为需要下载的文件
-		String fileName = request.getSession().getServletContext().getRealPath("upload")+File.separator+"myfile.txt";
+		//String fileName = request.getSession().getServletContext().getRealPath("upload")+File.separator+"myfile.txt";
+		String fileName = request.getSession().getServletContext().getRealPath("upload")+File.separator+file;
+		//System.out.println("---"+fileName);
 		//获取输入流
 		InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
 		//假如以中文名下载的话
-		String filename = "下载文件.txt";
+		//String filename = "下载文件.txt";
+		Date date = new Date();
+		SimpleDateFormat sdf  =  new SimpleDateFormat("YYYYMMddhhmmss");
+		String now = sdf.format(date);
+		String filename = now+"_"+file;
 		//转码 免得文件名中文乱码
-		filename = URLEncoder.encode(filename, "utf-8");
+		filename = URLEncoder.encode(filename, "UTF-8");
+		System.out.println("---"+filename);
 		//设置文件下载头
 		response.addHeader("Content-Disposition", "attachment;filename=" + filename);
 		//1.设置文件ContentType类型,这样设置,会自动判断下载文件类型
